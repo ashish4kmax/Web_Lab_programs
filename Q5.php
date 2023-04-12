824ac20001be15ddb15e2e15698a93e066c3ee40
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-04-12 10:23:27 +0530

    Update Q5.php

--- Q5.php
+++ Q5.php
@@ -4,4 +4,22 @@ echo "<h1>REFERESH PAGE</h1>";
 $c = file_get_contents($file);
 file_put_contents($file,$c+1);
 echo "Total views : ".$c;
-?>
+?><?php 
+
+ print"<h3>Refresh Page</h3>"; 
+
+ $name="counter.txt"; 
+
+ $file=fopen($name,"r"); 
+
+ $hits=fscanf($file,"%d"); 
+
+ fclose($file); 
+
+ $hits[0]++; 
+
+ $file=fopen($name,"w"); 
+
+ fprintf($file,"%d",$hits[0]);fclose($file); 
+
+ print("total no. of visits:".$hits[0]);
